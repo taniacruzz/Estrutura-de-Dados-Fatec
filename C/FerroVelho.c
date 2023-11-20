c793d5e068a67010bf2a92696f879dc685b0a0cf
taniacruzz <tanicruz112@gmail.com>
2023-11-19 23:54:46 -0300

    feat: FerroVelho - Pilha com cabeça

--- C/FerroVelho.c
+++ C/FerroVelho.c
@@ -11,9 +11,9 @@ typedef struct Car {
     struct Car* baseCar;
 } Car;
 
-Car* createCar() {
+Car* createCar(int weight) {
     Car* p_car = (Car*)malloc(sizeof(Car));
-    p_car->weight = rand() % 1000;
+    p_car->weight = weight;
     p_car->baseCar = NULL;
     return p_car;
 }
@@ -24,8 +24,7 @@ CarPile* createCarPile () {
     return p_pile;
 }
 
-void insertCar (CarPile* pPile) {
-    Car* p = createCar();
+void insertCar (Car* p, CarPile* pPile) {
     p->baseCar=pPile->topCar;
     pPile->topCar = p;
 }
@@ -36,19 +35,77 @@ void deleteCar (CarPile* pPile) {
     free(p_reserva);
 }
 
+// tira o carro da pilha sem deletá-lo.
 Car* takeOutCar (CarPile* pPile) {
     Car* p_reserva = pPile->topCar;
     pPile->topCar = pPile->topCar->baseCar;
+    p_reserva->baseCar = NULL;
     return p_reserva;
 }
 
-void moveCar(CarPile* p_origin, CarPile* p_destiny)
+void moveCar(CarPile* p_origin, CarPile* p_destiny) {
+    if (p_origin->topCar == NULL) {return;}
+    if (p_destiny->topCar != NULL && (p_destiny->topCar)->weight < (p_origin->topCar)->weight) {return;}
+    Car* p_removedCar = takeOutCar(p_origin);
+    insertCar(p_removedCar, p_destiny);
+}
+
+void populatePile(CarPile* p_Pile, int nCars, int baseWeight) {
+    insertCar(createCar(baseWeight), p_Pile);
+    if (nCars>1) {populatePile(p_Pile, nCars-1, baseWeight - 2);}
+}
+
+void printPile(Car* p_car) {
+    if (p_car == NULL) {printf(" |______| \n");}
+    else if (p_car->baseCar != NULL) {
+        printf(" |__%d__| \n", p_car->weight);
+        printPile(p_car->baseCar);
+    } else {
+        printf(" |__%d__| \n", p_car->weight);
+    }
+}
+
+void printGame(Car* pCar_finalPile, Car* pCar_startPile, Car* pCar_middlePile) {
+    printf("Configuracao do Ferro Velho\n\n");
+    printPile(pCar_startPile);
+    printf("----------------------------\n");
+    printPile(pCar_middlePile);
+    printf("----------------------------\n");
+    printPile(pCar_finalPile);
+    printf("\n\n");
+    
+}
 
 
 int main(){
-    Car* finalPile = createCarPile();
-    Car* startPile = createCarPile();
-    Car* middlePile = createCarPile();
+    CarPile* finalPile = createCarPile();
+    CarPile* startPile = createCarPile();
+    CarPile* middlePile = createCarPile();
+
+    populatePile(finalPile, 3, 20);
+    populatePile(startPile, 1, 19);
+    printGame(finalPile->topCar, startPile->topCar, middlePile->topCar);
+
+    moveCar(finalPile, startPile);
+    printGame(finalPile->topCar, startPile->topCar, middlePile->topCar);
+   
+    moveCar(finalPile, middlePile);
+    printGame(finalPile->topCar, startPile->topCar, middlePile->topCar);
+
+    moveCar(startPile, middlePile);
+    printGame(finalPile->topCar, startPile->topCar, middlePile->topCar);
+    
+    moveCar(startPile, finalPile);
+    printGame(finalPile->topCar, startPile->topCar, middlePile->topCar);
+    
+    moveCar(middlePile, startPile);
+    printGame(finalPile->topCar, startPile->topCar, middlePile->topCar);
+    
+    moveCar(middlePile, finalPile);
+    printGame(finalPile->topCar, startPile->topCar, middlePile->topCar);
+    
+    moveCar(startPile, finalPile);
+    printGame(finalPile->topCar, startPile->topCar, middlePile->topCar);
 
 
 }
